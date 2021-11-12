3001d3ad8fb1d8f627d8e098c5bf375ed39bcf22
renato <felicio95.jr@gmail.com>
2021-11-12 20:23:05 -0300

    :recycle: :boom: Refactoring at VehicleRegistry and Application.
    Decreasing declouping by taking the following actions:
    	- Grouping information logically. Vehicle and VehicleInfo attributes was distributed along the Application Class
    	- This directly implied to increase cohesion at VehicleRegistry. In order to register a Vehicle one needs a Vehicle. It seems to me, at this point like Dependency Inversion and Dependency Injection using the factory design pattern.

--- cohesion-coupling.py
+++ cohesion-coupling.py
@@ -9,17 +9,17 @@ import random
 
 
 class VehicleInfo:
-	def __init__(self):
-		self.brand: str = ''
-		self.catalog_price: int = 0
-		self.electric: bool = Fase
+	def __init__(self, brand: str, catalogue_price: int, electric: bool):
+		self.brand: str = brand
+		self.catalogue_price: int = catalogue_price
+		self.electric: bool = electric
 
 
 class Vehicle:
-	def __init__(self):
-		self.id: str = ''
-		self.license_plate: str = ''
-		self.info: VehicleInfo = VehicleInfo()
+	def __init__(self, id: str, license_plate: str, vehicle_info: VehicleInfo):
+		self.id: str = id
+		self.license_plate: str = license_plate
+		self.info: VehicleInfo = vehicle_info
 
 
 class VehicleRegistry:
@@ -28,12 +28,30 @@ class VehicleRegistry:
 		- Add a constructor method, defining the following attributes: lenght, id, vehicle license. This are the attributes of the vehicle.
 		- Then decouple all methods.
 	"""
+	def __init__(self):
+		self.vehicle_info: dict = {}
+		self.add_vehicle_info(brand="Tesla Model 3", catalogue_price=60000, electric=True)
+		self.add_vehicle_info(brand="Volkswagen ID3", catalogue_price=35000, electric=True)
+		self.add_vehicle_info(brand="BMW 5", catalogue_price=5000, electric=False)
+		self.add_vehicle_info(brand="Tesla Model Y", catalogue_price=5000, electric=True)
+
+	def add_vehicle_info(self, brand: string, catalogue_price: int, electric: bool) -> None:
+		self.vehicle_info[brand]: VehicleInfo = VehicleInfo(brand=brand, catalogue_price=catalogue_price, electric=electric)
+
 	def generate_vehicle_id(self, lenght) -> str:
         	return ''.join(random.choices(string.ascii_uppercase, k=lenght))
 
 	def generate_vehicle_license(self, id) -> str:
         	return f"{id[:2]}-{''.join(random.choices(string.digits, k=2))}-{''.join(random.choices(string.ascii_uppercase, k=2))}"
 
+	def create_vehicle(self, brand: str):
+		vehicle_id: str = self.generate_vehicle_id(12)
+		license_plate: str = self.generate_vehicle_license(vehicle_id)
+		return Vehicle(
+			id=vehicle_id,
+			license_plate=license_plate,
+			vehicle_info=self.vehicle_info[brand]
+		)
 
 class Application:
 	def register_vehicle(self, brand: str) -> None:
@@ -41,12 +59,7 @@ class Application:
 		# TODO: Remove this coupling between Application and VehicleRegistry
 		registry: VehicleRegistry = VehicleRegistry()
 		
-		# Creates a vehicle id of lenght 12
-		vehicle_id: str = registry.generate_vehicle_id(12)
-
-		# Creates a license plate for the vehicle
-		# using the frist two characters of the vehicle id
-		license_plate: str = registry.generate_vehicle_license(vehicle_id)
+		vehicle: Vehicle = registry.create_vehicle(brand)
 
 		# Computes the catalogue price
 		# TODO: Remove Coupling between data
@@ -76,8 +89,8 @@ class Application:
 		# Print out the vehicle registration information
 		print("Registration Complete. Vehicle Information:")
 		print(f"Brand: {brand}")
-		print(f"Id: {vehicle_id}")
-		print(f"License Plate{license_plate}")
+		print(f"Id: {vehicle.id}")
+		print(f"License Plate{vehicle.license_plate}")
 		print(f"Payable Tax: {payable_tax}")
 
 app = Application()
