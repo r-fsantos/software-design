aea590f40f6434925dfd2102d6c4b75ed6466d2f
renato <felicio95.jr@gmail.com>
2021-11-13 11:05:37 -0300

    :hammer: Addin ElectricPowerSwitch and LightBulb classes.
    ElectricPowerSwitch has a high coupling and therefore dependency on LightBulb.
    To solve this issue, dificult maintenence of such code, the Dependency Inversion Principle
    (D in SOLID OOP Design Principle), should be applied.
    
    It will be implemented by means of a Generic Interface class (Swichtable).
    This class should then be inherited by an LightBulb class, that should implement some generic methods.
    
    Interface classes are not native in pyhton, so one must use the abc package.
    More specifically, abstract classes should be put in place.

---
+++ 1-dependency-inversion/light_bulb_switch.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""
+Dev Notes: This file implements some code to practice
+	- Interfaces
+	- Dependency inversion
+"""
+
+
+class LightBulb:
+	def __init__(self) -> None:
+		pass
+
+	def turn_on(self) -> None:
+		print("LightBulb: turned on...")
+
+	def turn_off(self) -> None:
+		print("LightBulb: turned off...")
+
+
+class ElectricPowerSwitch:
+	def __init__(self, light_bulb: LightBulb) -> None:
+		self.light_bulb: LightBulb = light_bulb
+		self.on: bool = False
+
+	def press(self) -> None:
+		if self.on:
+			self.light_bulb.turn_off()
+			self.on = False
+		else:
+			self.light_bulb.turn_on()
+			self.on = True
+
+
+if __name__ == '__main__':
+	light_bulb: LightBulb = LightBulb() 
+	electric_power_switch: ElectricPowerSwitch = ElectricPowerSwitch(light_bulb=light_bulb) 
+	electric_power_switch.press()
+	electric_power_switch.press()
+	electric_power_switch.press()
+	electric_power_switch.press()	
